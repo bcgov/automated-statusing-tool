66b40c4b8fe40b6d457444357ea01689959f8bdf
wburt <william.burt@gov.bc.ca>
2026-04-13 16:45:54 -0700

    first attempt

---
+++ ast/core/results.py
@@ -0,0 +1,5 @@
+'''
+This module defines the authoritative structure of summary results produced
+by the runtime engine. It is responsible for assembling, validating, and
+serializing execution outputs into a predictable, schema-conformant form
+'''
