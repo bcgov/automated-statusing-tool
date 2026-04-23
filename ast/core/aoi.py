3aec5452b6b78e90b3348ab14c24bec096ddac16
wburt <william.burt@gov.bc.ca>
2026-04-23 09:31:42 -0700

    what does aoi do

---
+++ ast/core/aoi.py
@@ -0,0 +1,10 @@
+"""
+This will contain the functionality for dealing with AOI's
+If code base growns >~400 lines move to /aoi and make it a module
+- read the aoi with appropriate data adapter 
+- apply validation rules
+- return a gdf that meets a predictable format for the overlay
+
+TODO: implement /utilities/ for spatial data type detection?
+
+"""
