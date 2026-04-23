c2eb4ab22374afac554d1f6c2e9cd8f1f2b6f684
wburt <william.burt@gov.bc.ca>
2026-04-23 09:56:10 -0700

    inital comments and examples

---
+++ ast/core/overlay.py
@@ -0,0 +1,7 @@
+"""
+core/overlay.py takes the output from AOI and overlays
+it against the data described in datasets.yaml
+- if this code gets big put it in its own core/overlay module
+- consider a future using asynco for data reads that might have I/O latency
+- minimize and optimize geometry operations
+"""
