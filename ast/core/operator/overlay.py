a1f5f2821b7b0515934cfc4c854be46c6f705a8e
wburt <william.burt@gov.bc.ca>
2026-04-30 13:10:16 -0700

    expand overlay into different operators

--- ast/core/operator/overlay.py
+++ ast/core/operator/overlay.py
@@ -1,5 +1,5 @@
 """
-core/overlay.py takes the output from AOI and overlays
+core/operator/overlay.py takes the output from AOI and overlays
 it against the data described in datasets.yaml
 - if this code gets big put it in its own core/overlay module
 - consider a future using asynco for data reads that might have I/O latency
