3b304f6f5f5a009b7da8f15e9dc29216715bbedb
wburt <william.burt@gov.bc.ca>
2026-05-01 13:55:35 -0700

    mess with adapters

--- ast/core/data_adapters/kml/adapter.py
+++ ast/core/data_adapters/kml/adapter.py
@@ -3,32 +3,26 @@
 
 from pathlib import Path
 import geopandas as gpd
-from data_adapters.base import BaseSpatialAdapter
+from data_adapters.base import BaseSpatialAdapter, ReadOptions
 from data_adapters.exceptions import DataReadError, DataCrsError
 
 
 class KMLAdapter(BaseSpatialAdapter):
     """
     Adapter for KML files.
-    - read kml
-    - return crs compliant gdf
     """
 
-    def read(
+    def _read_impl(
         self,
+        *,
         path: str | Path,
+        read_options: ReadOptions,
         layer: str | None = None,
-        target_crs: str | None = None,
+        **_,
     ) -> gpd.GeoDataFrame:
         try:
-            gdf = gpd.read_file(path, layer=layer)
+            return gpd.read_file(path, layer=layer)
         except Exception as exc:
             raise DataReadError(f"Failed to read KML: {path}") from exc
+        
 
-        # KML is usually WGS84, but be defensive
-        if gdf.crs is None:
-            raise DataCrsError
-        if target_crs:
-            gdf = gdf.to_crs(target_crs)
-
-        return gdf
