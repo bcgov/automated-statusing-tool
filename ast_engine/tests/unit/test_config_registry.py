5a4148eb164bbf334234213795dc5f274c9e97e9
wburt <william.burt@gov.bc.ca>
2026-06-12 10:06:20 -0700

    add basedataset model and hydration tests

--- ast_engine/tests/unit/test_config_registry.py
+++ ast_engine/tests/unit/test_config_registry.py
@@ -1,6 +1,7 @@
 from ast_engine.config.registry import enrichment, utils, models
-
 from pathlib import Path
+
+
 DATA_DICT = [
         {
             "name": "Mapsheet",
@@ -29,8 +30,28 @@ def test_util_hydrate_datasets():
 def test_util_load_yaml():
     ''' Test create Registry from yaml
     '''    
-    registry = utils.load_yaml(Path('./sample_registry.yaml'))
+    registry = utils.load_yaml(Path('./tests/data/sample_registry.yaml'))
     assert registry.version=="1.0"
     assert len(registry.datasets)==2
     assert registry.datasets[0].crs=="EPSG:3005"
    
+def test_hydrate_base_datasets():
+    ''' Test dataset hydration'''
+    indata = [DATA_DICT[0]]
+    dsets = utils.hydrate_base_datasets(indata)
+    assert len(dsets) == 1
+    assert dsets[0].name == DATA_DICT[0]["name"]
+    assert dsets[0].datasource == DATA_DICT[0]["datasource"]
+
+def test_registry_creation():
+    ''' Test registry creation'''
+    dsets = utils.hydrate_base_datasets(DATA_DICT)
+    registry_datasets = []
+    for d in dsets:
+        enrich_data = enrichment.Enrich(d)
+        enrich_data.enrich()
+        rd = enrich_data.build()
+        registry_datasets.append(rd)
+    output = models.Registry(version="0.1", datasets=registry_datasets)
+    assert output.version == "0.1"
+    assert len(output.datasets) == 2
