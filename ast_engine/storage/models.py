948522c731a03acb456075dacf30fbd4b4e72804
wburt <william.burt@gov.bc.ca>
2026-06-23 15:30:52 -0700

    add client connection config

--- ast_engine/storage/models.py
+++ ast_engine/storage/models.py
@@ -3,9 +3,15 @@
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal, Optional
+from botocore.config import Config
 
 # TODO: Decide if this literal should be centralized
 EnvironmentName = Literal["dev", "test", "prod"]
+
+# TODO: Should retries max_attempts and mode be configurable? Load from application config!
+MAX_RETRIES = 5
+RETRY_MODE = "standard" 
+
 
 
 @dataclass(frozen=True)
@@ -15,9 +21,13 @@ class StorageConfig:
     prefix: str = "ast-results"
 
     # S3-compatible options
+    access_id: Optional[str] = None
+    access_key: Optional[str] = None
+    use_ssl: bool = True
     endpoint_url: Optional[str] = None
-    region_name: Optional[str] = "ca-central-1"
-
+    config: Optional[Config] = Config(request_checksum_calculation="when_required",
+                   response_checksum_validation="when_required",
+                   retries={'max_attempts': MAX_RETRIES, 'mode': RETRY_MODE})
     # Useful for local/dev testing
     local_root: Optional[Path] = None
 
