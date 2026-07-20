66359a314aeef88df76fa893788f4ca1bca7c746
jordanjfoy <jordan.foy@gov.bc.ca>
2026-07-20 12:53:16 -0700

    Everything coming along nicely - added Jinja templates

--- ast_api_test/main.py
+++ ast_api_test/main.py
@@ -26,7 +26,10 @@ posts: list[dict] = [
 @app.get("/", include_in_schema=False)
 @app.get("/posts", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html")
+    return templates.TemplateResponse(
+        request, "home.html", 
+        {"posts": posts, "title": "Home" }
+    )
 
 
 @app.get("/api/posts")
