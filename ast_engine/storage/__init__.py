3325b114106d968c373700bbfef9ce22c1bfed48
wburt <william.burt@gov.bc.ca>
2026-07-24 13:08:52 -0700

    adjust for settings name change

--- ast_engine/storage/__init__.py
+++ ast_engine/storage/__init__.py
@@ -7,17 +7,17 @@ from .models import StorageConfig, JobStorageContext
 from .s3_writer import S3ResultsStorageWriter
 from .local_writer import LocalResultsStorageWriter
 from .writer import ResultsStorageWriter
-from ast_engine.config import application_config
+from ast_engine.config import settings
 
 def create_results_writer(context: JobStorageContext) -> ResultsStorageWriter:
-    backend = application_config.AST_RESULTS_STORAGE_BACKEND
+    backend = settings.AST_RESULTS_STORAGE_BACKEND
 
     config = StorageConfig(
-        bucket=application_config.AST_RESULTS_BUCKET,
-        environment=application_config.AST_RESULTS_ENV,
-        prefix=application_config.AST_RESULTS_PREFIX,
-        endpoint_url=application_config.AST_RESULTS_S3_ENDPOINT_URL,
-        local_root=Path(application_config.AST_RESULTS_LOCAL_ROOT).resolve(),
+        bucket=settings.AST_RESULTS_BUCKET,
+        environment=settings.AST_RESULTS_ENV,
+        prefix=settings.AST_RESULTS_PREFIX,
+        endpoint_url=settings.AST_RESULTS_S3_ENDPOINT_URL,
+        local_root=Path(settings.AST_RESULTS_LOCAL_ROOT).resolve(),
     )
 
     if backend == "s3":
