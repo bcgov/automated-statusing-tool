e71eff5bff34173757521ff8b8f3f23c27acb767
wburt <william.burt@gov.bc.ca>
2026-09-13 21:00:34 -0700

    update with storage settings for local and s3

--- ast_engine/config/settings.py
+++ ast_engine/config/settings.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
 from typing import Literal
 
 class Settings(BaseSettings):
     # App environment
     environment: Literal["development", "staging", "production"] = "development"
-    
+
     # Logging
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
     log_file: str | None = None
@@ -16,13 +17,33 @@ class Settings(BaseSettings):
     #S3 Connections
     s3_max_retries: int = 5
     s3_retry_mode: Literal["standard"] = "standard"
+    s3_use_ssl: bool = True
 
     # retain spatial results
     record_spatial: bool = False
 
     # temporary directory for storing results
     temp_dir: str | None = None
-    
+
+    # storage of results
+    deployment: Literal["DEV","TEST","PROD"] = "DEV"
+    storage_type: Literal["S3","local"] = "local"
+    results_local_root: str = 'burn-severity/local'
+    results_s3_endpoint_url: str = "http://local-s3:9000"
+    results_s3_access_id: str
+    results_s3_key: SecretStr | None = None
+    results_s3_bucket: str
+    results_prefix: str = "ast-results"
+
+    # redis
+    job_queue: str = "ast_job_queue"
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    redis_database: int = 0
+    redis_password: SecretStr | None = None
+
+    # engine settings
+    system_crs: str = "EPSG:3005"
 
     model_config = SettingsConfigDict(
         env_file=".env",
